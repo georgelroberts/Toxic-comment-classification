97362d7c3b1a86830dbcdd1bff53a4c06dec3674
George <georgelroberts@hotmail.com>
2018-01-21 21:34:20 -0200

    Very basic NB fit added
    No data cleaning, just a simple pipeline with a count vectoriser, TF IDF transformer and a multinomial naive bayes classifier.

--- toxicCommentClass.py
+++ toxicCommentClass.py
@@ -11,6 +11,8 @@ belong to multiple classes.
 
 TODO: Preliminary EDA on data.
 TODO: Add a comment length feature.
+TODO: Look at https://www.kdnuggets.com/2017/12/general-approach-preprocessing-
+      text-data.html for preprocessing text.
 TODO: See the most commonly occuring words when a message is classified as
       toxic.
 TODO: Run a simple pipeline using TF-IDF and a naive Bayes' classifier. Multi-
@@ -18,14 +20,36 @@ TODO: Run a simple pipeline using TF-IDF and a naive Bayes' classifier. Multi-
 """
 
 import pandas as pd
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.pipeline import Pipeline
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.model_selection import train_test_split, cross_val_score
 
 train = pd.read_csv("data/train.csv")
-test = pd.read_csv("data/test.csv")
+realTest = pd.read_csv("data/test.csv")
 sampleSub = pd.read_csv("data/sample_submission.csv")
+train = train.set_index('id')
+trainX = train[['comment_text']]
+trainY = train.drop('comment_text', axis=1)
 
-class_cols = [name for name in train.columns if name not in ['id',
-                                                             'comment_text']]
-classCounts = train[class_cols].sum()
-
+classes = list(trainY.columns)
+classCounts = trainY.sum()
 print("The classes and the number of comments classified as each is:\n" +
       classCounts.to_string())
+
+trainX, testX, trainY, testY = train_test_split(
+        trainX, trainY, test_size=0.33, random_state=42)
+
+textClf = Pipeline([('vect', CountVectorizer()),
+                    ('tfidf', TfidfTransformer()),
+                    ('clf', MultinomialNB())
+                    ])
+
+
+for column in classes:
+    textClf.fit(trainX['comment_text'], trainY[column])
+    print(cross_val_score(textClf, testX['comment_text'], testY[column], 
+                          verbose=1).mean())
+
+
+
