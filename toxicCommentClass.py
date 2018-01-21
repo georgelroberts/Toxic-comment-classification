212de9289be1715131e1792b832b8b420e666c88
George <georgelroberts@hotmail.com>
2018-01-21 16:26:41 -0200

    Initial commit

---
+++ toxicCommentClass.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jan 21 16:16:32 2018
+
+@author: George L. Roberts
+
+Use manually classified data from Wikipedia's talk page edits to classify 
+conversations according to different 'toxicity' features. 
+"""
+
