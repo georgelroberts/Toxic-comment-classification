e588fded844b45ee03bd8c7f3db2e6215d9ed313
George <georgelroberts@hotmail.com>
2018-01-24 21:39:28 -0200

    Functionalised EDA, cleaning and fitting. Added new cleaning features and some EDA.

--- toxicCommentClass.py
+++ toxicCommentClass.py
@@ -14,7 +14,7 @@ TODO: Preliminary EDA on data.
         * Message length
         * Count number of exclamation marks/question marks
 
-TODO: Remove IP addresses (and possibly user_ids). Maybe all numbers. 
+TODO: Remove IP addresses (and possibly user_ids). Maybe all numbers.
 TODO: See the most commonly occuring words when a message is classified as
       different things.
 TODO: Add submission of actual test data.
@@ -23,11 +23,12 @@ TODO: Add submission of actual test data.
 import pandas as pd
 import re
 import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+import random
 from sklearn_pandas import DataFrameMapper
 from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.naive_bayes import MultinomialNB
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import f1_score, confusion_matrix
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import log_loss
 
@@ -38,69 +39,79 @@ sampleSub = sampleSub.set_index('id')
 trainX = train[['comment_text']]
 trainX.columns = ['msg']
 trainY = train.drop('comment_text', axis=1)
+rTest = pd.read_csv("data/test.csv")
+rTest = rTest.set_index('id')
+rTest.columns = ['msg']
+
 classes = list(trainY.columns)
-
-
-def EDA(trainX, trainY):
-    """ All exploratory data analysis will be contained within here """
-    classCounts = trainY.sum()
-    print("The classes and the number of comments classified as each is:\n" +
-          classCounts.to_string())
 
 
 def addFeatures(data):
     """ Add extra features beyond the bag of n-grams """
-    data['pcCaps'] = data['msg'].apply(lambda x:
-                                        len(re.findall(r'[A-Z]', x))/len(x))
+    data = data.assign(pcCaps = data['msg'].apply(lambda x:
+                               len(re.findall(r'[A-Z]', x))/len(x)))
+    data = data.assign(noExclam = data['msg'].apply(lambda x:
+                                         len(re.findall(r'!', x))))
+    data = data.assign(noQues = data['msg'].apply(lambda x:
+                                len(re.findall(r'\?', x))))
     return data
 
 
-def removeURLs(data):
-    data = data.replace(r'http\S+', 'website', regex=True).\
-                replace(r'www.\S+', 'website', regex=True)
-    return data
+trainX = addFeatures(trainX)
+rTest = addFeatures(rTest)
 
 
-def removeSpecials(data):
-    charDict = {'msg': {'!': ' ', '"': ' ', '\n': ' ',
-                                 '@': ''}}
-    data = data.replace(charDict, regex=True)
-    return data
+def EDA(trainX, trainY, classes):
+    """ All exploratory data analysis will be contained within here """
+
+    # First look at the classes and their occurrences
+
+    classCounts = trainY.sum()
+    sns.barplot(classes, classCounts)
+
+    # Now lets extract some short random comments from each class
+
+    for vulgar in classes:
+        temp = trainX[trainY[vulgar] == 1]['msg']
+        temp = temp[temp.str.len() < 100]
+        noComments = 2
+        randomNos = random.sample(range(1, len(temp)), noComments)
+        print("Vulgarity: " + vulgar)
+        print(temp.iloc[randomNos].values)
+
+    # Look at how much the extra features I added above affect the
+    # classification.
+
+    a = trainX[['pcCaps', 'noExclam', 'noQues']].join(trainY)
+    corr = a.corr()
+    plt.matshow(corr, cmap=sns.diverging_palette(220, 10, as_cmap=True))
+    plt.xticks(range(len(corr.columns)), corr.columns)
+    plt.yticks(range(len(corr.columns)), corr.columns)
+    return corr
+
+
+# corr = EDA(trainX, trainY)
 
 
 def cleanData(data):
-    data = removeURLs(data)
-    data = removeSpecials(data)
+    """ Remove URLs, special characters and IP addresses """
+    data = data.replace(r'http\S+', 'website', regex=True).\
+           replace(r'www.\S+', 'website', regex=True) 
+
+    charDict = {'msg': {'!': ' ', '"': ' ', '\n': ' ', '@': ' '}}
+    # data = data.replace(charDict, regex=True)
+    
+    data = data.replace(r'\b(?:[0-9]{1,3}\.){3}[0-9]{1,3}\b', 'IP',
+                        regex=True)
+    
+    data = data.replace(r'[^a-zA-Z0-9 ]', ' ', regex=True)
+
     data['msg'] = data['msg'].astype(str)
     return data
 
 
 trainX = cleanData(trainX)
-trainX = addFeatures(trainX)
-
-# Create vectorizer for function to use. Thanks to stackexchange
-# https://stackoverflow.com/questions/30653642/combining-bag-of-words-and-other
-# -features-in-one-model-using-sklearn-and-pandas
-
-mapper = DataFrameMapper([
-    (['pcCaps'], None),
-    ('msg', TfidfVectorizer(binary=True, ngram_range=(1, 1),
-                                     min_df=0.0005, max_df=0.5))
-], sparse=True)
-trainXSparse = mapper.fit_transform(trainX)
-trainXSparseColumns = mapper.features[0][0] +\
-                      mapper.features[1][1].get_feature_names()
-
-# Load the submission data
-rTest = pd.read_csv("data/test.csv")
-rTest = rTest.set_index('id')
-rTest.columns = ['msg']
 rTest = cleanData(rTest)
-rTest = addFeatures(rTest)
-rTestSparse = mapper.transform(rTest)
-
-sTrainX, sTestX, sTrainY, sTestY = train_test_split(
-        trainXSparse, trainY, test_size=0.25, random_state=42)
 
 # fit from
 # https://www.kaggle.com/jhoward/nb-svm-strong-linear-baseline-eda-0-052-lb
@@ -118,16 +129,39 @@ def get_mdl(x, y):
     x_nb = x.multiply(r)
     return m.fit(x_nb, y), r
 
-score = []
 
-for column in classes:
-    m, r = get_mdl(sTrainX, sTrainY[column])
-    prediction = pd.DataFrame(m.predict_proba(sTestX.multiply(r))[:,1])
-    score.append(log_loss(sTestY[column], prediction))
-    m, r = get_mdl(trainXSparse, trainY[column])
-    sampleSub[column] = m.predict_proba(rTestSparse.multiply(r))[:,1]
+def fitData(classes, trainX, trainY, rTest):
+    """ Transform the data into a sparse matrix and fit it """
+    mapper = DataFrameMapper([
+            (['pcCaps'], None),
+            ('msg', TfidfVectorizer(binary=True, ngram_range=(1, 1),
+                                    min_df=0.0005, max_df=0.5))
+    ], sparse=True)
+    trainXSparse = mapper.fit_transform(trainX)
+    trainXSparseColumns = mapper.features[0][0] +\
+                          mapper.features[1][1].get_feature_names()
 
-print(score)
-print(np.mean(score))
+    rTestSparse = mapper.transform(rTest)
 
-sampleSub.to_csv("submissions/NB2.csv")
+    sTrainX, sTestX, sTrainY, sTestY = train_test_split(
+            trainXSparse, trainY, test_size=0.25, random_state=42)
+
+    score = []
+
+    for column in classes:
+        m, r = get_mdl(sTrainX, sTrainY[column])
+        prediction = pd.DataFrame(m.predict_proba(sTestX.multiply(r))[:, 1])
+        score.append(log_loss(sTestY[column], prediction))
+        m, r = get_mdl(trainXSparse, trainY[column])
+        sampleSub[column] = m.predict_proba(rTestSparse.multiply(r))[:, 1]
+
+    print(score)
+    print(np.mean(score))
+
+    sampleSub.to_csv("submissions/NB2.csv")
+
+    return score, sampleSub
+
+
+score, sampleSub = fitData(classes, trainX, trainY, rTest)
+    
